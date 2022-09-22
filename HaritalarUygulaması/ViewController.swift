db9bf046c431748fd14304afabb9170825468a31
Hamza Oban <hamzaoban3@gmail.com>
2022-09-22 21:38:27 +0300

    general additions
    location select function has been written.
    Annotation added.

--- HaritalarUygulaması/ViewController.swift
+++ HaritalarUygulaması/ViewController.swift
@@ -6,12 +6,56 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
 
+    @IBOutlet weak var isimTextField: UITextField!
+    
+    @IBOutlet weak var notTextField: UITextField!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        let gestureReconizer = UILongPressGestureRecognizer(target: self, action: #selector(konumSec(gestureRecognizer:)))
+        gestureReconizer.minimumPressDuration = 2
+        mapView.addGestureRecognizer(gestureReconizer)
+        
+    }
+    
+    @objc func konumSec(gestureRecognizer : UILongPressGestureRecognizer){
+        if gestureRecognizer.state == .began{
+            let dokunulanNokta = gestureRecognizer.location(in: mapView)
+            let dokunulanKoordinat = mapView.convert(dokunulanNokta, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = dokunulanKoordinat
+            annotation.title = isimTextField.text
+            annotation.subtitle = isimTextField.text
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.9, longitudeDelta: 0.9)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
     }
 
 
